c23aa75d0386da34b3a4ea3e7fcc9bfbfe4cf14c
shokhsanam18 <shokhsanam18@gmail.com>
2024-12-29 23:06:08 +0500

    navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
-
+import Navbar from "./components/nav"
 
 function App() {
  
 
   return (
     <>
+   <Navbar/>
    
     </>
   )
---
+++ src/components/nav.jsx
@@ -0,0 +1,25 @@
+import { useState } from "react";
+import './nav.css'
+import { AlignJustify } from "lucide-react";
+import { BellDot } from "lucide-react";
+import Photo from "../assets/people19.png";
+
+const Navbar = () => {
+  const [nav, setNav] = useState(false);
+  return (
+    <nav className="NavBar">
+      <button className="SideBarMenu">
+        <AlignJustify className="Menu"/>
+      </button>
+      <div className="right">
+        <button className="Bell">
+          <BellDot />
+        </button>
+        <img src={Photo} alt="this is image"  />
+      </div>
+      
+    </nav>
+  );
+};
+
+export default Navbar;
