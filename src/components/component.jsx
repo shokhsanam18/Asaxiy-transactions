56a2f550f733518f5ccac4a9e7db41e05eabd1eb
shirina-t <shirina.tillyabaeva@gmail.com>
2024-12-29 14:36:01 +0500

    blablabla

--- src/components/component.jsx
+++ src/components/component.jsx
@@ -2,13 +2,9 @@ const Component = () => {
   return (
     <div>
       <h1>hello GitHub</h1>
-      <h2>Pull Push </h2>
       <h3>Shirin</h3>
-<<<<<<< HEAD
-      <p>Does my branch effect Developers?</p>
-=======
-      <h4>yana ozgartirdim</h4>
->>>>>>> 164198209d0275601c00cdb3c365d212c398d5fc
+      <h2>Pull Push </h2>
+      <p>hello world</p>
     </div>
   );
 };
