1edf9c3710503386b3565a96f1b54e7990c49821
shokhsanam18 <shokhsanam18@gmail.com>
2024-12-29 15:32:53 +0500

    hello

--- src/components/component.jsx
+++ src/components/component.jsx
@@ -1,10 +1,7 @@
 const Component = () => {
   return (
     <div>
-      <h1>hello GitHub</h1>
-      <h2>Pull Push </h2>
-      <h3>Shirin</h3>
-      <h4>yana ozgartirdim</h4>
+ 
     </div>
   );
 };
