ce70692610b91ad3eb006ffbcf5dd13d8a827f07
shokhsanam18 <shokhsanam@gmail.com>
2024-12-31 14:31:49 +0500

    i did

---
+++ src/components/analytics.jsx
@@ -0,0 +1,5 @@
+const Analytics = () => {
+  return <h1>Analiz</h1>;
+};
+
+export default Analytics;
--- src/components/nav.jsx
+++ src/components/nav.jsx
@@ -1,30 +1,45 @@
 // import { useState } from "react";
-import './nav.css'
+import "./nav.css";
 import { AlignJustify } from "lucide-react";
 import { BellDot } from "lucide-react";
 import Photo from "../assets/people19.png";
-
+import Logo from "../assets/asaxiy-logo.svg";
+import { Link } from "react-router-dom";
 
 const Navbar = () => {
   // const [nav, setNav] = useState(false);
 
-
-
   return (
     <>
-    <nav className="NavBar">
-      <button className="SideBarMenu" >
-        <AlignJustify className="Menu"/>
-      </button>
-      <div className="right">
-        <button className="Bell">
-          <BellDot />
+      <nav className="NavBar">
+        <button className="SideBarMenu">
+          <AlignJustify className="Menu" />
         </button>
-        <img src={Photo} alt="this is image"  />
+        <div className="right">
+          <button className="Bell">
+            <BellDot />
+          </button>
+          <img src={Photo} alt="this is image" className="photo" />
+        </div>
+      </nav>
+      <div className="SideBar">
+        <div className="logo">
+          <a href="/">
+            <img src={Logo} alt="" />
+          </a>
+        </div>
+        <ul>
+          <li>
+            <Link to="/tahlil">Tahlil</Link>
+          </li>
+          <li>
+            <Link to="/conversions">Valyuta kursi</Link>
+          </li>
+          <li>
+            <Link to="/transactions">Transaktsiya boshqaruvi</Link>
+          </li>
+        </ul>
       </div>
-    </nav>
-    <div className="SideBar">
-    </div>
     </>
   );
 };
