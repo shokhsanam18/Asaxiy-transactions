e151ebab2fe05aa90d4ce68cd387e30d9b67af97
shokhsanam18 <shokhsanam@gmail.com>
2025-01-01 14:06:55 +0500

    all

--- src/components/nav.jsx
+++ src/components/nav.jsx
@@ -10,16 +10,16 @@ import Logo from "../assets/asaxiy-logo.svg";
 import { Link } from "react-router-dom";
 
 const Navbar = () => {
-  const [side, setSideBar] = useState(false);
+  const [side, setSideBar] = useState(true);
 
   const SideBarShow = () => setSideBar(!side);
 
   return (
     <>
-      <nav className="NavBar sticky top-0 flex shadow-sm p-5 h-[90px] justify-between items-center flex-wrap">
+      <nav className="NavBar sticky top-0 flex shadow-sm p-5 h-[90px] justify-between items-center flex-wrap flex-shrink">
         <div className="flex items-center justify-center">
           <button
-            className="SideBarMenu w-16 h-11 bg-neutral-100 border-none rounded-md flex justify-center items-center hover:bg-gray-100"
+            className="SideBarMenu w-16 h-11 bg-neutral-100 border-none rounded-md flex justify-center items-center fixed left-96 hover:bg-gray-100"
             onClick={SideBarShow}
           >
             <AlignJustify className="Menu text-gray-800 hover:text-gray-600" />
@@ -41,7 +41,7 @@ const Navbar = () => {
       </nav>
 
       {side ? (
-        <div className="SideBar h-screen w-[320px] fixed top-0 left-0 bg-neutral-100 flex p-5 gap-10 flex-col ">
+        <div className="SideBar h-screen w-[320px] fixed top-0 left-0 bg-neutral-100 flex p-5 gap-10 flex-col translate-x-0 transition-all">
           <div className="logo">
             <a href="/">
               <img src={Logo} alt="" />
@@ -73,7 +73,40 @@ const Navbar = () => {
             </li>
           </ul>
         </div>
-      ) : null}
+      ) : (
+        <div className="SideBar h-screen w-[320px] fixed top-0 left-0 bg-neutral-100 flex p-5 gap-10 flex-col -translate-x-full transition-all">
+          <div className="logo">
+            <a href="/">
+              <img src={Logo} alt="" />
+            </a>
+          </div>
+          <ul className="list-none no-underline flex justify-center items-start flex-col gap-4">
+            <li className=" w-[260px] ">
+              <Link to="/tahlil">
+                <button className="font-bold  bg-blue-600 text-white rounded-md flex items-center justify-start gap-3 p-3 text-xs uppercase w-full hover:shadow-xl">
+                  <ChartPie className="inline-block" />
+                  Tahlil
+                </button>
+              </Link>
+            </li>
+            <li className="w-[260px]">
+              <Link to="/conversions">
+                <button className="font-bold  bg-blue-600 text-white rounded-md flex items-center justify-start gap-3 p-3 text-xs uppercase w-full hover:shadow-xl">
+                  <CircleDollarSign /> Valyuta kursi
+                </button>
+              </Link>
+            </li>
+            <li className="w-[260px]">
+              <Link to="/transactions">
+                <button className="font-bold bg-blue-600 text-white rounded-md flex items-center justify-start gap-3 p-3 text-xs uppercase w-full hover:shadow-xl">
+                  <ArrowLeftRight />
+                  Transaktsiya boshqaruvi
+                </button>
+              </Link>
+            </li>
+          </ul>
+        </div>
+      )}
     </>
   );
 };
