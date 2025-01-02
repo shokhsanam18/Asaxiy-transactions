be35ec1ec08173805d5653a7b3d9e92213c3b418
shokhsanam18 <shokhsanam@gmail.com>
2025-01-02 19:25:47 +0500

    sd

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { Route, Routes } from "react-router-dom";
 import { Layout } from "./components/layout";
-import Transactions from "./components/transactions";
+import Transactions from "./components/Transactions";
 import Conversions from "./components/conversions";
 import Analytics from "./components/analytics";
 
