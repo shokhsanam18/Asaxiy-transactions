e15bbe6b4b0af4ee59b99d4d941100732d99783b
shokhsanam18 <shokhsanam@gmail.com>
2025-01-03 17:40:25 +0500

    miniSidebar

---
+++ src/components/miniSidebar.jsx
@@ -0,0 +1,35 @@
+import React from "react";
+import { ArrowLeftRight } from "lucide-react";
+import { ChartPie } from "lucide-react";
+import { CircleDollarSign } from "lucide-react";
+import { Link } from "react-router-dom";
+
+export const MiniSidebar = () => {
+  return (
+    <div className="bg-blue-600">
+      <ul className="list-none no-underline flex justify-center items-center  gap-3">
+        <li>
+          <Link to="/tahlil">
+            <button className="font-bold  text-white  w-full">
+              <ChartPie />
+            </button>
+          </Link>
+        </li>
+        <li>
+          <Link to="/conversions">
+            <button className="font-bold text-white w-full">
+              <CircleDollarSign />
+            </button>
+          </Link>
+        </li>
+        <li>
+          <Link to="/transactions">
+            <button className="font-bold text-white  w-full">
+              <ArrowLeftRight />
+            </button>
+          </Link>
+        </li>
+      </ul>
+    </div>
+  );
+};
